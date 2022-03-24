036d8733845481ef84f11fea062f0b3c3fa9fb45
Rayan Azhari <rayan.azhari.17@ucl.ac.uk>
2022-03-24 18:04:45 +0000

    minor revision

--- scr/data_load.R
+++ scr/data_load.R
@@ -3,42 +3,26 @@ library(tidyverse)
 library(data.table)
 
 
-
-
-# Load data from CSVs -----------------------------------------------------
+# Source code -------------------------------------------------------------
 
 # load functions by sourcing script files
 source(here::here("scr", "def_functions.R"))
-
-
-# Load non-domestic EPC certificate and recommendations
-nd_epc_cert <- load_EPC_certficates("all-non-domestic-certificates")
-nd_epc_recom <- load_EPC_recommendations("all-non-domestic-certificates")
-
-# Load domestic EPC certificate and recommendations
-n_epc_cert <- load_EPC_certficates("all-domestic-certificates")
-n_epc_recom <- load_EPC_recommendations("all-domestic-certificates")
-
-# Load DEC certificate and recommendations
-dec_cert <- load_DEC_certficates("all-display-certificates")
-# Load DEC certificate and recommendations
-dec_recom <- load_DEC_recommendations("all-display-certificates")
-
-
-
-
-
-# Write to databse --------------------------------------------------------
 
 # load functions by sourcing script files
 source(here::here("scr", "connect_to_sql_db.R"))
 
 
+# Non-Domestic EPC -----------------------------------------------------
+
+# Load non-domestic EPC certificate and recommendations
+nd_epc_cert <- load_EPC_certficates("all-non-domestic-certificates")
+
 #Write nd_epc_cert to the database
 dbWriteTable(
   con,
-  DBI::Id(schema  = "EPC",table   = "nd_epc_cert"),
-  value = nd_epc_cert, # dataframe to write to DB
+  DBI::Id(schema  = "EPC", table   = "nd_epc_cert"),
+  value = nd_epc_cert,
+  # dataframe to write to DB
   append = FALSE,
   row.names = FALSE
 )
@@ -46,11 +30,15 @@ dbWriteTable(
 rm(nd_epc_cert)
 
 
+nd_epc_recom <-
+  load_EPC_recommendations("all-non-domestic-certificates")
+
 #Write nd_epc_recom to the database
 dbWriteTable(
   con,
-  DBI::Id(schema  = "EPC",table   = "nd_epc_recom"),
-  value = nd_epc_recom, # dataframe to write to DB
+  DBI::Id(schema  = "EPC", table   = "nd_epc_recom"),
+  value = nd_epc_recom,
+  # dataframe to write to DB
   append = FALSE,
   row.names = FALSE
 )
@@ -58,11 +46,19 @@ dbWriteTable(
 rm(nd_epc_recom)
 
 
+
+# Domestic EPC ------------------------------------------------------------
+
+# Load domestic EPC certificate and recommendations
+n_epc_cert <- load_EPC_certficates("all-domestic-certificates")
+
+
 #Write n_epc_cert to the database
 dbWriteTable(
   con,
-  DBI::Id(schema  = "EPC",table   = "n_epc_cert"),
-  value = n_epc_cert, # dataframe to write to DB
+  DBI::Id(schema  = "EPC", table   = "n_epc_cert"),
+  value = n_epc_cert,
+  # dataframe to write to DB
   append = FALSE,
   row.names = FALSE
 )
@@ -70,11 +66,14 @@ dbWriteTable(
 rm(n_epc_cert)
 
 
+n_epc_recom <- load_EPC_recommendations("all-domestic-certificates")
+
 #Write n_epc_recom to the database
 dbWriteTable(
   con,
-  DBI::Id(schema  = "EPC",table   = "n_epc_recom"),
-  value = n_epc_recom, # dataframe to write to DB
+  DBI::Id(schema  = "EPC", table   = "n_epc_recom"),
+  value = n_epc_recom,
+  # dataframe to write to DB
   append = FALSE,
   row.names = FALSE
 )
@@ -83,22 +82,33 @@ rm(nd_epc_recom)
 
 
 
+# DEC ---------------------------------------------------------------------
+
+# Load DEC certificate and recommendations
+dec_cert <- load_DEC_certficates("all-display-certificates")
+
 #Write dec_cert to the database
 dbWriteTable(
   con,
-  DBI::Id(schema  = "DEC",table   = "dec_cert"),
-  value = dec_cert, # dataframe to write to DB
+  DBI::Id(schema  = "DEC", table   = "dec_cert"),
+  value = dec_cert,
+  # dataframe to write to DB
   append = FALSE,
   row.names = FALSE
 )
 
 rm(dec_cert)
 
+
+# Load DEC certificate and recommendations
+dec_recom <- load_DEC_recommendations("all-display-certificates")
+
 #Write dec_recom to the database
 dbWriteTable(
   con,
-  DBI::Id(schema  = "DEC",table   = "dec_recom"),
-  value = dec_recom, # dataframe to write to DB
+  DBI::Id(schema  = "DEC", table   = "dec_recom"),
+  value = dec_recom,
+  # dataframe to write to DB
   append = FALSE,
   row.names = FALSE
 )
