a7aaa6fcb9b7725179d3e81845858048182565bb
Rayan Azhari <rayan.azhari.17@ucl.ac.uk>
2022-03-24 17:59:16 +0000

    connect to DB and write tables

--- scr/data_load.R
+++ scr/data_load.R
@@ -5,6 +5,8 @@ library(data.table)
 
 
 
+# Load data from CSVs -----------------------------------------------------
+
 # load functions by sourcing script files
 source(here::here("scr", "def_functions.R"))
 
@@ -14,10 +16,8 @@ nd_epc_cert <- load_EPC_certficates("all-non-domestic-certificates")
 nd_epc_recom <- load_EPC_recommendations("all-non-domestic-certificates")
 
 # Load domestic EPC certificate and recommendations
-nd_epc_cert <- load_EPC_certficates("all-domestic-certificates")
-nd_epc_recom <- load_EPC_recommendations("all-domestic-certificates")
-
-
+n_epc_cert <- load_EPC_certficates("all-domestic-certificates")
+n_epc_recom <- load_EPC_recommendations("all-domestic-certificates")
 
 # Load DEC certificate and recommendations
 dec_cert <- load_DEC_certficates("all-display-certificates")
@@ -25,3 +25,82 @@ dec_cert <- load_DEC_certficates("all-display-certificates")
 dec_recom <- load_DEC_recommendations("all-display-certificates")
 
 
+
+
+
+# Write to databse --------------------------------------------------------
+
+# load functions by sourcing script files
+source(here::here("scr", "connect_to_sql_db.R"))
+
+
+#Write nd_epc_cert to the database
+dbWriteTable(
+  con,
+  DBI::Id(schema  = "EPC",table   = "nd_epc_cert"),
+  value = nd_epc_cert, # dataframe to write to DB
+  append = FALSE,
+  row.names = FALSE
+)
+
+rm(nd_epc_cert)
+
+
+#Write nd_epc_recom to the database
+dbWriteTable(
+  con,
+  DBI::Id(schema  = "EPC",table   = "nd_epc_recom"),
+  value = nd_epc_recom, # dataframe to write to DB
+  append = FALSE,
+  row.names = FALSE
+)
+
+rm(nd_epc_recom)
+
+
+#Write n_epc_cert to the database
+dbWriteTable(
+  con,
+  DBI::Id(schema  = "EPC",table   = "n_epc_cert"),
+  value = n_epc_cert, # dataframe to write to DB
+  append = FALSE,
+  row.names = FALSE
+)
+
+rm(n_epc_cert)
+
+
+#Write n_epc_recom to the database
+dbWriteTable(
+  con,
+  DBI::Id(schema  = "EPC",table   = "n_epc_recom"),
+  value = n_epc_recom, # dataframe to write to DB
+  append = FALSE,
+  row.names = FALSE
+)
+
+rm(nd_epc_recom)
+
+
+
+#Write dec_cert to the database
+dbWriteTable(
+  con,
+  DBI::Id(schema  = "DEC",table   = "dec_cert"),
+  value = dec_cert, # dataframe to write to DB
+  append = FALSE,
+  row.names = FALSE
+)
+
+rm(dec_cert)
+
+#Write dec_recom to the database
+dbWriteTable(
+  con,
+  DBI::Id(schema  = "DEC",table   = "dec_recom"),
+  value = dec_recom, # dataframe to write to DB
+  append = FALSE,
+  row.names = FALSE
+)
+
+rm(dec_recom)
